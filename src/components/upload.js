312fe8e67c9dcc6e2f21928bff7f0f3f0631709e
SyedObaid <obaidquadri@hotmail.com>
2021-04-08 15:38:32 -0500

    background

--- src/components/upload.js
+++ src/components/upload.js
@@ -2,10 +2,11 @@ import React from "react";
 
 const Upload = () => {
   return (
-    <div class="container">
-      <div class="row" style={{ marginTop: "15%" }}>
+
+    <div class="container"  >
+      <div class="row">
         <div class="col text-center align-middle">
-          <h1 style={{textAlign: 'center'}}>Welcome to Single Syllabus</h1>
+          <h1 style={{ textAlign: 'center' }}>Welcome to Single Syllabus</h1>
           <br />
           <br />
           <form action="/action_page.php">
