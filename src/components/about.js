525c49713e4a552e1d8a49bfba35e3f03df16de6
Saaketh Jasti <saakethkjasti@outlook.com>
2021-04-10 17:28:11 -0500

    throwaway

--- src/components/about.js
+++ src/components/about.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import divya from '../pictures/divya.jpg';
-import leo from '../pictures/leo.jpg';
-import syed from '../pictures/syed.jpg';
-import saaketh from '../pictures/saaketh.jpg';
-=======
 import React from "react";
 import divya from "../pictures/divya.jpg";
 import leo from "../pictures/leo.jpg";
 import syed from "../pictures/syed.jpg";
 import saaketh from "../pictures/saaketh.jpg";
->>>>>>> df0066b27f69b6fa9a9e05aa1365a93f83a2da25
 
 const About = () => {
   return (
@@ -33,52 +25,6 @@ const About = () => {
         </div>
       </div>
 
-<<<<<<< HEAD
-  <h1 className="members">Members</h1>
-  
-  <div className="card-deck">
-  <div className="card">
-    <img className="card-img-top" src={leo}alt="Card cap"></img>
-    <div className="card-body">
-      <h5 className="card-title">Leonardo Acioli</h5>
-      <p className="card-text">From: Recife, Brazil​</p>
-       <p className="card-text">Interest: Web Development</p>
-        <p className="card-text">Position: Software Engineer intern​</p>
-    </div>
-     <div className="card-footer">Fun Fact: Has done Ballet for 6 years​</div>
-</div>
-  <div className="card">
-    <img className="card-img-top" src={divya} alt="Card cap"></img>
-    <div className="card-body">
-      <h5 className="card-title">Divya Karivaradasamy</h5>
-      <p className="card-text">From: Cupertino, CA​</p>
-       <p className="card-text">Interest: Web Development</p>
-        <p className="card-text">Position: Software Engineer​</p>
-    </div>
-     <div className="card-footer">Fun Fact: Has played the violin for 12 years</div>
-    </div>
-  <div className="card">
-    <img className="card-img-top" src={saaketh} alt="Card cap"></img>
-    <div className="card-body">
-      <h5 className="card-title">Saaketh Jasti</h5>
-      <p className="card-text">From: Irving, TX​</p>
-       <p className="card-text">Interest: Web/App development</p>
-        <p className="card-text">Position: Technology Intern (Infrastructure)​</p>
-    </div>
-    <div className="card-footer">Fun Fact: Plays acoustic guitar</div>
-</div>
-  <div className="card d-flex flex-nowrap">
-    <img className="card-img-top" src={syed} alt="Card cap"></img>
-    <div className="card-body">
-      <h5 className="card-title">Syed Quadri</h5>
-      <p className="card-text">From: Dammam, Saudi Arabia​</p>
-       <p className="card-text">Interest: Data Science</p>
-        <p className="card-text">Position: Data Engineer</p>
-    </div>
-     <div className="card-footer">Fun Fact: Likes solving puzzles </div>
-     </div>
-    </div>
-=======
       <h1 className="members">Members</h1>
 
       <div className="card-deck">
@@ -133,7 +79,6 @@ const About = () => {
           <div className="card-footer">Fun Fact: Likes solving puzzles </div>
         </div>
       </div>
->>>>>>> df0066b27f69b6fa9a9e05aa1365a93f83a2da25
     </div>
   );
 };
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,102 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
-import Upload from "./upload";
-import About from "./about";
-import Contact from "./contact";
-import Help from "./help";
-import Edit from "./edit"
-
-const Navbar = () => {
-  return (
-    <Router>
-      <div>
-        <nav
-          className="navbar sticky-top navbar-expand-lg navbar-light"
-          style={{ backgroundColor: "orange", color: "black" }}
-        >
-          <a className="navbar-brand" href="index.php">
-            <strong>Single Syllabus</strong>
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <Link to="/classes" className="nav-link">
-                  Classes
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/" className="nav-link">
-                  Upload
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/user" className="nav-link">
-                  User
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/contact" className="nav-link">
-                  Contact
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/help" className="nav-link">
-                  ?
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/about" className="nav-link">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link to="/signout" className="nav-link">
-                  Sign Out
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <Switch>
-          <Route path="/classes">
-            <About />
-          </Route>
-          <Route path="/user">
-            <About />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/help">
-            <Help />
-          </Route>
-          <Route path="/edit">
-            <Edit />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/signout">
-            <About />
-          </Route>
-          <Route path="/">
-            <Upload />
-          </Route>
-        </Switch>
-=======
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
@@ -157,7 +59,6 @@ const Navbar = () => {
             </Link>
           </li>
         </ul>
->>>>>>> df0066b27f69b6fa9a9e05aa1365a93f83a2da25
       </div>
     </nav>
   );
