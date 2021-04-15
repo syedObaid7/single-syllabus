3d21a2e8e75cc3b62b00aa84099fe3bbb4488ee9
Leonardo Acioli Arruda Sampaio <65642158+LeoAcioli@users.noreply.github.com>
2021-04-15 01:27:42 -0500

    l

--- test.py
+++ test.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-@app.route("/")
-def index():
-   return ('/upload')
+@app.route ("/")
+def index()
+   return render_template("upload.html")
 
 @app.route('/upload',methods=['POST'])
 def upload():
    target = os.path.join(APP_ROOT, 'PDFS/')
    print(target)
 
-   if not os.path.isdir(target)
+   if not os.path.isdir(target):
       os.mkdir(target)
 
-   for file in request.files.getlist("file")
+   for file in request.files.getlist("file"):
       print(file)
       filename = file.filename
       destination = "/".join([target, filename])
